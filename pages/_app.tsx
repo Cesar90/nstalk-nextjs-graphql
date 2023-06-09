858a2043c0b9db3f09fe1de26ff433c22a1a9950
Cesar <cesarc@nicasource.com>
2023-06-09 10:20:19 -0600

    Disable eslint duplicate next/app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import '@assets/main.css'
 import type { AppProps } from 'next/app';
+/* eslint-disable */
 import App, { AppContext } from 'next/app';
 import type { NextPageContext } from 'next'
 import { ApolloProvider, ApolloClient } from '@apollo/client';
+
 // import Router from 'next/router';
 // import NProgress from 'nprogress'
 // import 'nprogress/nprogress.css'
@@ -22,7 +24,7 @@ function MyApp({ Component, pageProps, apollo }: AppProps & { apollo: ApolloClie
     </ApolloProvider>
   )
 }
-
+/* eslint-enable */
 MyApp.getInitialProps = async (ctx: NextPageContext & AppContext) => {
   const appProps = await App.getInitialProps(ctx);
   return { ...appProps };
