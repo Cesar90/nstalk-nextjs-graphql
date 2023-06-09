45d003432511e9ad7f2225ce75db37c2bcd8d676
Cesar <cesarc@nicasource.com>
2023-06-09 12:17:56 -0600

    Layout.spect.tsx fixed deploy

--- components/__test__/Layout.spec.tsx
+++ components/__test__/Layout.spec.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-// import { render, screen, waitFor } from '@testing-library/react'
-import { render } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
-// import Router from 'next/router' // We will MOCK THIS
+import Router from 'next/router' // We will MOCK THIS
 import Layout from '../Layout'
-// import { isLoggedInVar } from '../../lib/withData'
+import { isLoggedInVar } from '../../lib/withData'
 
 // jest.mock('@apollo/client', () => ({
 //   makeVar: () => {
@@ -41,26 +40,25 @@ jest.mock('next/router', () => ({
 
 describe('<Layout>', () => {
   it('renders OK', async () => {
-    render(<Layout>test</Layout>)
-    // const { container } = render(<Layout>test</Layout>)
+    const { container } = render(<Layout>test</Layout>)
     // debug();
-    // await waitFor(() => {
-    //   isLoggedInVar(true)
-    // })
-    // const success = await screen.findByText(/test/i)
-    // expect(success).toBeInTheDocument()
-    // const text = screen.getByText('test')
-    // expect(text).toHaveTextContent('test')
-    // expect(container).toHaveTextContent('test')
+    await waitFor(() => {
+      isLoggedInVar(true)
+    })
+    const success = await screen.findByText(/test/i)
+    expect(success).toBeInTheDocument()
+    const text = screen.getByText('test')
+    expect(text).toHaveTextContent('test')
+    expect(container).toHaveTextContent('test')
   })
 
   it('renders no OK', async () => {
     render(<Layout>test</Layout>)
-    // await waitFor(() => {
-    //   isLoggedInVar(true)
-    // })
+    await waitFor(() => {
+      isLoggedInVar(true)
+    })
     // debug();
-    // expect(Router.push).toHaveBeenCalled()
-    // expect(Router.push).toHaveBeenCalledWith({ pathname: '/' })
+    expect(Router.push).toHaveBeenCalled()
+    expect(Router.push).toHaveBeenCalledWith({ pathname: '/' })
   })
 })
